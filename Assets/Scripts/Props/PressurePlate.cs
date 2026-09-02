88680f022ffe24fbc71041f0523af1642b2813e4
DavidRuedaLo <148922742+DavidRuedaLo@users.noreply.github.com>
2026-09-01 20:42:43 -0500

    01-09-26 -- 3
    - Added event-driven pressure plate
    - Added animations for plate

---
+++ Assets/Scripts/Props/PressurePlate.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressurePlate : MonoBehaviour
+{
+    private Animator anim;
+    public enum PlateState
+    {
+        Off = 0,
+        Activate =  1,
+        On = 2,
+        Deactivate = 3,
+    }
+
+    //hashset to track unique colliders and avoid double-counting
+    private HashSet<Collider> occupants = new HashSet<Collider>();
+
+    public UnityEvent onActivate;
+    public UnityEvent onDeactivate;
+
+    private bool isActive;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("PushableBox"))
+        {
+            if (occupants.Count == 0)
+            {
+                TogglePlate();
+            }
+
+            Debug.Log("player entered switch");
+
+            occupants.Add(other);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(occupants.Contains(other))
+        {
+            occupants.Remove(other);
+
+            if (occupants.Count == 0)
+            {
+                TogglePlate();
+            }
+        }
+    }
+
+    public void TogglePlate()
+    {
+        isActive = !isActive;
+
+        if (isActive)
+        {   
+            anim.SetInteger("PlateState", (int)PlateState.Activate);
+            onActivate?.Invoke();
+        }
+        else
+        {   
+            anim.SetInteger("PlateState", (int)PlateState.Deactivate);
+            onDeactivate?.Invoke();
+        }
+    }
+}
