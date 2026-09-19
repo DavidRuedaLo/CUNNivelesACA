e703a849ed4e283e51ed615c66bb78a8edab3a48
dangomezgir <daniel.gomezgiraldo@hotmail.com>
2026-09-19 14:59:08 -0500

    19-09-26
    - Started patrol zone logic. Needs refining.

--- Assets/Scripts/Enemy/AIManager.cs
+++ Assets/Scripts/Enemy/AIManager.cs
@@ -8,7 +8,7 @@ public class AIManager : MonoBehaviour
     public float detectRadius, attackRadius;
     public float rotationSpeed;
     public LayerMask layer;
-    private bool isAttacking = false;
+    private bool isAttacking = false, inPatrolZone = true;
     public NavMeshAgent agent;
     public Transform[] waypoints;
     public int currentWaypoint = 0;
@@ -20,18 +20,21 @@ public class AIManager : MonoBehaviour
 
     public Weapon gun;
 
-    private Coroutine shoot;
+    private Coroutine shoot, returnToZone;
 
-
-    void Start()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("PatrolZone"))
+        {
+            Debug.Log("Exited patrol zone");
+            inPatrolZone = false;
+        }
     }
 
-    
     void Update()
     {
         CheckState();
+        Debug.Log("Current state: " + currentState + "\nInPatrolZone: " + inPatrolZone);
     }
 
     public void CheckState()
@@ -40,24 +43,39 @@ public class AIManager : MonoBehaviour
         switch (currentState)
         {
             case State.Patrol:
+                agent.isStopped = false;
                 agent.stoppingDistance = 0f;
                 Patrol();
                 if (distance <= detectRadius) currentState = State.Pursuit;
                 break;
 
             case State.Pursuit:
-                agent.SetDestination(player.position);
-                if (distance <= attackRadius) currentState = State.Attack;
-                else if (distance > detectRadius)
+                if (inPatrolZone)
+                {
+                    agent.isStopped = false;
+                    agent.SetDestination(player.position);
+                    Aim(distance);
+                    if (distance <= attackRadius) currentState = State.Attack;
+                    if (distance > detectRadius)
+                    {
+                        agent.SetDestination(waypoints[currentWaypoint].position);
+                        currentState = State.Patrol;
+                    }
+                }
+                else
                 {
                     agent.SetDestination(waypoints[currentWaypoint].position);
-                    currentState = State.Patrol;
+                    returnToZone = StartCoroutine(ReturnToZone());
+                    //if (!agent.pathPending)
+                    //{
+                    //}
                 }
                 break;
 
             case State.Attack:
                 if (distance > attackRadius) currentState = State.Pursuit;
-                Attack(distance);
+                Aim(distance);
+                Attack();
                 break;
 
             default:
@@ -75,13 +93,17 @@ public class AIManager : MonoBehaviour
         }
     }
 
-    void Attack(float dist)
+    void Aim(float dist)
     {
-        agent.stoppingDistance = attackRadius - 0.1f;
-        //transform.LookAt(player);
         Vector3 targetDirection = player.position - transform.position;
         float speed = rotationSpeed / dist * rotationSpeed;
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(targetDirection), speed * Time.deltaTime);
+    }
+
+    void Attack()
+    {
+        agent.stoppingDistance = attackRadius - 0.1f;
+        
 
         if (Physics.Raycast(gun.firePoint.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, attackRadius, layer, QueryTriggerInteraction.Ignore)){
             if (!isAttacking && hit.collider.gameObject.CompareTag("Player")) shoot = StartCoroutine(Shoot());
@@ -97,6 +119,13 @@ public class AIManager : MonoBehaviour
         gun.Fire();
         yield return new WaitForSeconds(1f);
         isAttacking = false;
+    }
+
+    public IEnumerator ReturnToZone()
+    {
+        yield return new WaitForSeconds(2f);
+        currentState = State.Patrol;
+        inPatrolZone = true;
     }
 
     private void OnDrawGizmosSelected()
